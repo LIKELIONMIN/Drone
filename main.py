f402487d897c4db1c740c8df8ef705214d13972f
unknown <apollo96@naver.com>
2019-11-13 20:42:19 +0900

    left right cw ccw

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 myTello = tello.Tello()
 
 myTello.sendMsg("command")
-myTello.sendMsg("takeoff")
 
+myTello.sendMsg("takeoff")
 time.sleep(10)
 
 myTello.up(30)
@@ -13,5 +13,16 @@ time.sleep(5)
 
 myTello.down(30)
 time.sleep(5)
+
+myTello.left(30)
+time.sleep(5)
+
+myTello.cw(30)
+time.sleep(5)
+
+myTello.ccw(30)
+time.sleep(5)
+
+
 myTello.sendMsg("land")
 print ("Well Done")
--- tello.py
+++ tello.py
@@ -13,7 +13,7 @@ class Tello:
         self.tello_address = ('192.168.10.1', 8889)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(self.locaddr)
-        self.recvThread = threading.Thread(target=self.recv)
+        self.recvThread = threading.Thread(target=self.recv, daemon=True)
         self.recvThread.start()
 
     def recv(self):
@@ -48,3 +48,34 @@ class Tello:
         if x< 20:
             x= 20
         self.sendMsg("down " + str(x)) 
+
+    def left(self, x):
+        if x> 500 :
+            x= 500
+        if x< 20:
+            x= 20
+        self.sendMsg("left " + str(x))
+
+    def right(self, x):
+        if x> 500 :
+            x= 500
+        if x< 20:
+            x= 20
+        self.sendMsg("right " + str(x))
+
+    def cw(self, x):
+        if x> 360 :
+            x= 360
+        if x< 1:
+            x= 1
+        self.sendMsg("cw " + str(x))
+
+    def ccw(self, x):
+        if x> 360 :
+            x= 360
+        if x< 1:
+            x= 1
+        self.sendMsg("ccw " + str(x)) 
+          
+
+      
